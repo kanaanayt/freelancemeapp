4bd196807cf58d2029093b7db1d00d4a52ec9de6
kanaanayt <202101747@pmu.edu.sa>
2024-06-12 08:54:51 +0300

    s

---
+++ src/FMA.Application/Migrations/20240612053409_Mig6.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FMA.Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class Mig6 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Freelancers",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateRegistered", "PhotoPath" },
+                values: new object[] { new DateTime(2024, 4, 13, 0, 0, 0, 0, DateTimeKind.Utc), "https://freelanceme.blob.core.windows.net/container1/m1" });
+
+            migrationBuilder.UpdateData(
+                table: "Freelancers",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateRegistered", "PhotoPath" },
+                values: new object[] { new DateTime(2024, 3, 14, 0, 0, 0, 0, DateTimeKind.Utc), "https://freelanceme.blob.core.windows.net/container1/m2" });
+
+            migrationBuilder.UpdateData(
+                table: "Freelancers",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "DateRegistered", "PhotoPath" },
+                values: new object[] { new DateTime(2024, 4, 28, 0, 0, 0, 0, DateTimeKind.Utc), "https://freelanceme.blob.core.windows.net/container1/m3" });
+
+            migrationBuilder.UpdateData(
+                table: "Freelancers",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "DateRegistered", "PhotoPath" },
+                values: new object[] { new DateTime(2024, 3, 29, 0, 0, 0, 0, DateTimeKind.Utc), "https://freelanceme.blob.core.windows.net/container1/m4" });
+
+            migrationBuilder.UpdateData(
+                table: "Freelancers",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "DateRegistered", "PhotoPath" },
+                values: new object[] { new DateTime(2024, 3, 4, 0, 0, 0, 0, DateTimeKind.Utc), "https://freelanceme.blob.core.windows.net/container1/m5" });
+
+            migrationBuilder.UpdateData(
+                table: "Freelancers",
+                keyColumn: "Id",
+                keyValue: 6,
+                columns: new[] { "DateRegistered", "PhotoPath" },
+                values: new object[] { new DateTime(2024, 4, 18, 0, 0, 0, 0, DateTimeKind.Utc), "https://freelanceme.blob.core.windows.net/container1/m6" });
+
+            migrationBuilder.UpdateData(
+                table: "Freelancers",
+                keyColumn: "Id",
+                keyValue: 7,
+                columns: new[] { "DateRegistered", "PhotoPath" },
+                values: new object[] { new DateTime(2024, 3, 19, 0, 0, 0, 0, DateTimeKind.Utc), "https://freelanceme.blob.core.windows.net/container1/m7" });
+
+            migrationBuilder.UpdateData(
+                table: "Freelancers",
+                keyColumn: "Id",
+                keyValue: 8,
+                columns: new[] { "DateRegistered", "PhotoPath" },
+                values: new object[] { new DateTime(2024, 4, 3, 0, 0, 0, 0, DateTimeKind.Utc), "https://freelanceme.blob.core.windows.net/container1/m8" });
+
+            migrationBuilder.UpdateData(
+                table: "Freelancers",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "DateRegistered",
+                value: new DateTime(2024, 2, 23, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Freelancers",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "DateRegistered",
+                value: new DateTime(2024, 3, 9, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Freelancers",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "DateRegistered",
+                value: new DateTime(2024, 3, 24, 0, 0, 0, 0, DateTimeKind.Utc));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Freelancers",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateRegistered", "PhotoPath" },
+                values: new object[] { new DateTime(2024, 4, 12, 0, 0, 0, 0, DateTimeKind.Utc), "/images/nadia.jpg" });
+
+            migrationBuilder.UpdateData(
+                table: "Freelancers",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateRegistered", "PhotoPath" },
+                values: new object[] { new DateTime(2024, 3, 13, 0, 0, 0, 0, DateTimeKind.Utc), "/images/khalid.jpg" });
+
+            migrationBuilder.UpdateData(
+                table: "Freelancers",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "DateRegistered", "PhotoPath" },
+                values: new object[] { new DateTime(2024, 4, 27, 0, 0, 0, 0, DateTimeKind.Utc), "/images/sara.jpg" });
+
+            migrationBuilder.UpdateData(
+                table: "Freelancers",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "DateRegistered", "PhotoPath" },
+                values: new object[] { new DateTime(2024, 3, 28, 0, 0, 0, 0, DateTimeKind.Utc), "/images/yousef.jpg" });
+
+            migrationBuilder.UpdateData(
+                table: "Freelancers",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "DateRegistered", "PhotoPath" },
+                values: new object[] { new DateTime(2024, 3, 3, 0, 0, 0, 0, DateTimeKind.Utc), "/images/layla.jpg" });
+
+            migrationBuilder.UpdateData(
+                table: "Freelancers",
+                keyColumn: "Id",
+                keyValue: 6,
+                columns: new[] { "DateRegistered", "PhotoPath" },
+                values: new object[] { new DateTime(2024, 4, 17, 0, 0, 0, 0, DateTimeKind.Utc), "/images/omar.jpg" });
+
+            migrationBuilder.UpdateData(
+                table: "Freelancers",
+                keyColumn: "Id",
+                keyValue: 7,
+                columns: new[] { "DateRegistered", "PhotoPath" },
+                values: new object[] { new DateTime(2024, 3, 18, 0, 0, 0, 0, DateTimeKind.Utc), "/images/huda.jpg" });
+
+            migrationBuilder.UpdateData(
+                table: "Freelancers",
+                keyColumn: "Id",
+                keyValue: 8,
+                columns: new[] { "DateRegistered", "PhotoPath" },
+                values: new object[] { new DateTime(2024, 4, 2, 0, 0, 0, 0, DateTimeKind.Utc), "/images/fahad.jpg" });
+
+            migrationBuilder.UpdateData(
+                table: "Freelancers",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "DateRegistered",
+                value: new DateTime(2024, 2, 22, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Freelancers",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "DateRegistered",
+                value: new DateTime(2024, 3, 8, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Freelancers",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "DateRegistered",
+                value: new DateTime(2024, 3, 23, 0, 0, 0, 0, DateTimeKind.Utc));
+        }
+    }
+}
